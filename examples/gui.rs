ece697b8c1647fc3d7609c67d6af04785bd04bbf
Evan Pratten <ewpratten@gmail.com>
2022-04-12 21:13:13 -0400

    An imporoved GUI demo

--- examples/gui.rs
+++ examples/gui.rs
@@ -16,15 +16,45 @@ fn main() {
     // Clear the display
     display.clear_screen().unwrap();
 
+    // Keep track of the second page
+    let mut is_in_info_page = false;
+
     // Components
     let mut title_bar = TitleBar::new("kobo-rs GUI demo", TextAlign::Center);
     let title_cycle_button = Button::new("Cycle title text", CharSpaceCoord::new(3, 7), None);
+    let info_page_button = Button::new("More info", CharSpaceCoord::new(25, 7), None);
+    let quit_button = Button::new("Quit", CharSpaceCoord::new(3, 12), None);
 
     // Loop and let the user cycle through demos
     loop {
+
         // Render everything
         title_bar.render(&mut display).unwrap();
-        title_cycle_button.render(&mut display).unwrap();
+        if !is_in_info_page {
+            title_cycle_button.render(&mut display).unwrap();
+            info_page_button.render(&mut display).unwrap();
+            quit_button.render(&mut display).unwrap();
+        } else {
+            // Render info
+            display
+                .write_str(
+                    "This is a demo of the kobo-rs GUI library.",
+                    CharSpaceCoord::new(1, 7),
+                    false,
+                    None,
+                    None,
+                )
+                .unwrap();
+            display
+                .write_str(
+                    "Not a whole lot on its own, but useful for making simple apps on top of Nickel", 
+                    CharSpaceCoord::new(1, 9), 
+                    false, 
+                    None, 
+                    None
+                )
+                .unwrap();
+        }
 
         // Wait for a screen tap and check the button actions
         loop {
@@ -32,16 +62,47 @@ fn main() {
                 .next_touch(display.get_screen_size_px(), None)
                 .unwrap();
 
-            // Handle the title style change button
-            if title_cycle_button.is_within(&touch.position, &display) {
-                // Cycle the title bar alignment
-                title_bar.set_text_align(match title_bar.get_text_align() {
-                    TextAlign::Left => TextAlign::Center,
-                    TextAlign::Center => TextAlign::Right,
-                    TextAlign::Right => TextAlign::Left,
-                });
+            if !is_in_info_page {
+                // Handle the title style change button
+                if title_cycle_button.is_within(&touch.position, &display) {
+                    println!("Cycling title text style");
+                    // Cycle the title bar alignment
+                    title_bar.set_text_align(match title_bar.get_text_align() {
+                        TextAlign::Left => TextAlign::Center,
+                        TextAlign::Center => TextAlign::Right,
+                        TextAlign::Right => TextAlign::Left,
+                    });
+                    break;
+                }
+
+                // Handle the info page button
+                if info_page_button.is_within(&touch.position, &display) {
+                    println!("Switching to info page");
+                    is_in_info_page = true;
+
+                    // Refresh the screen
+                    display.clear_screen().unwrap();
+                    break;
+                }
+
+                // Handle the quit button
+                if quit_button.is_within(&touch.position, &display) {
+                    println!("Quitting");
+
+                    // Refresh the screen
+                    display.clear_screen().unwrap();
+                    return;
+                }
+            } else {
+                // In the info page, a tap brings us back
+                println!("Switching to main page");
+                is_in_info_page = false;
+
+                // Refresh the screen
+                display.clear_screen().unwrap();
                 break;
             }
         }
     }
+
 }
